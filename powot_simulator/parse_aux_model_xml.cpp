3b22573a8fa0aefb3695e684cf9dd276daaa5184
Lubomir Bogdanov <lbogdanov@tu-sofia.bg>
2018-12-09 16:55:16 +0200

    auxiliary_model_file: XML is now fully parsed. Values are not yet loaded to the proper structures of the simulator.

--- powot_simulator/parse_aux_model_xml.cpp
+++ powot_simulator/parse_aux_model_xml.cpp
@@ -21,6 +21,8 @@
 #include "powotsimulator.h"
 
 void powotsimulator::parse_aux_model_xml(void){
+    QString tag_text;
+
     qDebug()<<"(powotsimulator) Parsing auxiliary model XML file ...";
 
     QFile file(bin_model_aux_xml_file);
@@ -31,14 +33,126 @@ void powotsimulator::parse_aux_model_xml(void){
         if (reader.name() == "auxiliary_model"){
             while(reader.readNextStartElement()){
                 if(reader.name() == "metrics"){
-                    QString s = reader.readElementText();
-                    qDebug()<<"-->"<<s;
+                    tag_text = reader.readElementText();
+                    qDebug()<<"-->"<<tag_text;
                 }
                 else if(reader.name() == "divisor"){
-                    QString s = reader.readElementText();
-                    qDebug()<<"++>"<<s;
+                    tag_text = reader.readElementText();
+                    qDebug()<<"++>"<<tag_text;
                 }
-                else{
+                else if(reader.name() == "domains"){
+                    while(reader.readNextStartElement()){
+                        if(reader.name() == "memory"){
+                            while(reader.readNextStartElement()){
+                                if(reader.name() == "flash_memory"){
+                                    while(reader.readNextStartElement()){
+                                        if(reader.name() == "start"){
+                                            tag_text = reader.readElementText();
+                                            qDebug()<<"++>flash_memory_start: "<<tag_text;
+                                        }
+                                        else if(reader.name() == "end"){
+                                            tag_text = reader.readElementText();
+                                            qDebug()<<"++>flash_memory_end: "<<tag_text;
+                                        }
+                                    }
+                                }
+                                else if(reader.name() == "sram_memory"){
+                                    while(reader.readNextStartElement()){
+                                        if(reader.name() == "start"){
+                                            tag_text = reader.readElementText();
+                                            qDebug()<<"++>sram_memory_start: "<<tag_text;
+                                        }
+                                        else if(reader.name() == "end"){
+                                            tag_text = reader.readElementText();
+                                            qDebug()<<"++>sram_memory_end: "<<tag_text;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        else if(reader.name() == "temperatures"){
+                            while(reader.readNextStartElement()){
+                                if(reader.name() == "temperature"){
+                                    tag_text = reader.readElementText();
+                                    qDebug()<<"++>temperature: "<<tag_text;
+                                }
+                            }
+                        }
+                        else if(reader.name() == "voltages"){
+                            while(reader.readNextStartElement()){
+                                if(reader.name() == "voltage"){
+                                    tag_text = reader.readElementText();
+                                    qDebug()<<"++>voltage: "<<tag_text;
+                                }
+                            }
+                        }
+                        else if(reader.name() == "frequencies"){
+                            while(reader.readNextStartElement()){
+                                if(reader.name() == "frequency"){
+                                    tag_text = reader.readElementText();
+                                    qDebug()<<"++>frequency: "<<tag_text;
+                                }
+                            }
+                        }
+                        else if(reader.name() == "number_of_operands"){
+                            while(reader.readNextStartElement()){
+                                if(reader.name() == "operands"){
+                                    tag_text = reader.readElementText();
+                                    qDebug()<<"++>operands: "<<tag_text;
+                                }
+                            }
+                        }
+                    }
+                }
+                else if(reader.name() == "dvs_apis"){
+                    while(reader.readNextStartElement()){
+                        if(reader.name() == "dvs_api"){
+                            while(reader.readNextStartElement()){
+                                if(reader.name() == "dvs_api_prototype"){
+                                    tag_text = reader.readElementText();
+                                    qDebug()<<"++>dvs_api_prototype: "<<tag_text;
+                                }
+                                else if(reader.name() == "voltage"){
+                                    tag_text = reader.readElementText();
+                                    qDebug()<<"++>voltage: "<<tag_text;
+                                }
+                                else if(reader.name() == "exec_time"){
+                                    tag_text = reader.readElementText();
+                                    qDebug()<<"++>exec_time: "<<tag_text;
+                                }
+                                else if(reader.name() == "energy"){
+                                    tag_text = reader.readElementText();
+                                    qDebug()<<"++>energy: "<<tag_text;
+                                }
+                            }
+                        }
+                    }
+                }
+                else if(reader.name() == "dfs_apis"){
+                    while(reader.readNextStartElement()){
+                        if(reader.name() == "dfs_api"){
+                            while(reader.readNextStartElement()){
+                                if(reader.name() == "dfs_api_prototype"){
+                                    tag_text = reader.readElementText();
+                                    qDebug()<<"++>dfs_api_prototype: "<<tag_text;
+                                }
+                                else if(reader.name() == "frequency"){
+                                    tag_text = reader.readElementText();
+                                    qDebug()<<"++>frequency: "<<tag_text;
+                                }
+                                else if(reader.name() == "exec_time"){
+                                    tag_text = reader.readElementText();
+                                    qDebug()<<"++>exec_time: "<<tag_text;
+                                }
+                                else if(reader.name() == "energy"){
+                                    tag_text = reader.readElementText();
+                                    qDebug()<<"++>energy: "<<tag_text;
+                                }
+                            }
+                        }
+                    }
+                }
+                else{                    
                     reader.skipCurrentElement();
                 }
             }
